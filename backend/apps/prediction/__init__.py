10391cfc52ff6b1360f6502a83fbbb8a8e8bd03f
MukBambot118 <134835507+MukarramBambot@users.noreply.github.com>
2025-11-06 03:33:51 +0530

    Delete backend directory

--- backend/apps/prediction/__init__.py
+++
@@ -1,1 +0,0 @@
-# prediction app package marker
--- backend/apps/prediction/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PredictionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.apps.prediction'
-    verbose_name = 'Prediction'
